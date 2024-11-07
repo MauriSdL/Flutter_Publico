c40de65f7df8d688ad4652ec81b282f5cd2c9793
MauriSdL <iruam_mauri@msn.com>
2024-11-07 10:17:43 -0300

    Curso Dart aula 15 Operadores Aritimeticos

---
+++ Curso_Flutter/Dart_Aulas/aula_14_variaveis.dart
@@ -0,0 +1,23 @@
+
+
+// Comentario de uma linha
+
+/*
+
+  * Comentario de
+  * varias linhas
+
+ */
+
+
+
+void main() {
+  int idade = 30;
+  double distancia = 24.89;
+  String nome = "Mauri";
+
+  print(idade);
+  print(nome);
+  print(distancia);
+  
+}
---
+++ Curso_Flutter/Dart_Aulas/aula_15_Operadores_aritimeticos.dart
@@ -0,0 +1,29 @@
+
+// ignore_for_file: unused_local_variable
+
+import 'dart:math';
+
+void main() {
+  double num1 = 20;
+  double num2 = 17;
+  int num3 = 3;
+
+  double soma = num1 + num2;
+  print(soma);
+
+  double subtracao = num1 - num2;
+  print(subtracao);
+  
+  double multiplicacao = num1 * num2;
+  print(multiplicacao);
+  
+  double divisao = num1 / num2;
+  print(divisao);
+  
+  num exponenciacao = pow(num1, num2);
+  print(exponenciacao);
+  
+  double resta_Divisao = num1 % num2;
+  print(resta_Divisao);
+
+}
